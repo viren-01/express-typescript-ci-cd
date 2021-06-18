cc2fb356a9630a2223f5bb21c376f09a9fab5582
viren-01 <71268813+viren-01@users.noreply.github.com>
2021-06-18 11:07:40 +0530

    Update config.ts

--- config/config.ts
+++ config/config.ts
@@ -2,5 +2,4 @@ export default{
     port: 3000,
     host: 'localhost',
     //dbUri: "mongodb://localhost:27017/express-typescript-pipeline"
-    dbUri: "mongodb://msrhwlabdb:8JFXw9DOlWiueGhgyLOIEcKg6nGaELdW8xWpBhjfTKQyhwlGvlx3ZIZjfHL0eO5JGsauQz54zPixZtVA6yT3ug==@msrhwlabdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@msrhwlabdb@"
 }
