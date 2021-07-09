3d56955e5fe277ea73abda55e9b9a47b69273f5f
Virendra Vishwakarma <vishwakarmavirendra74@gmail.com>
2021-07-09 15:53:47 +0530

    Updating Scripts

--- src/app.ts
+++ src/app.ts
@@ -30,7 +30,7 @@ try {
 
     const server = http.createServer(app);
     const io = new Server(server);
-    let timer = 59;
+    let timer = 100;
     setInterval(() => {
         timer = timer - 1;
     }, 1000)
